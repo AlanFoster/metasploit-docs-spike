6ba31d4ea604fdd6285020d5940071c11e7b2aef
alanfoster <alan@alanfoster.me>
2020-05-09 23:18:15 +0100

    Add gh-pages

---
+++ precache-manifest.b6937e76a6c7ff675228b1effd8bf0d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe7625e798be8924af20741dcbf574c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46825a2fcb9aeda10d2a",
+    "url": "./static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "41c5a08a9c60112cc82b",
+    "url": "./static/css/main.a6ee1414.chunk.css"
+  },
+  {
+    "revision": "46825a2fcb9aeda10d2a",
+    "url": "./static/js/2.80afd4e4.chunk.js"
+  },
+  {
+    "revision": "41c5a08a9c60112cc82b",
+    "url": "./static/js/main.1ee222c3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6503398916f6ed1207b8f0624280b09e",
+    "url": "./static/media/logo.65033989.svg"
+  }
+]);
