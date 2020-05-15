022a56b851e6e5dd7d0a682ef0244259459348c4
alanfoster <alan@alanfoster.me>
2020-05-16 00:55:50 +0100

    Move gatsby folder contents back into gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,9 +4,65 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path');
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-module.exports = {
-  createPages: require('./gatsby/createPages'),
-  onCreateNode: require('./gatsby/onCreateNode'),
-}
+// Replacing '/' would result in empty string which is invalid
+const replacePath = (path) => path === `/` ? path : path.replace(/\/$/, ``);
+
+/**
+ * Creating wiki pages
+ */
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const WikiTemplate = path.resolve(`src/templates/wiki-template.tsx`);
+  return graphql(`
+    {
+      allMdx {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+    result.data.allMdx.edges.forEach(({ node }) => {
+      createPage({
+        path: replacePath(node.fields.slug),
+        component: WikiTemplate,
+        context: { id: node.id }, // additional data can be passed via context
+      })
+    })
+  })
+};
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: replacePath(slug),
+    })
+  } else if (node.internal.type === 'Mdx') {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      // Name of the field you are adding
+      name: 'slug',
+      // Individual MDX node
+      node,
+      // Generated value based on filepath with "blog" prefix
+      // value: `/blog${value}`,
+      value: replacePath(value),
+    })
+  }
+};
